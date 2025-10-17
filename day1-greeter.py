976d732b3d3faf6f3bc91b3aa43ee6e1e659d474
Classic-Trev <classic.trev@proton.me>
2025-10-17 17:48:08 -0500

    Add files via upload
    Day 1: Hello World and basic programs
    
    - Completed Python for Everybody Weeks 1-2
    - Learned variables, expressions, operators
    - Built 5 working programs
    - First day of learning journey!

---
+++ day1-greeter.py
@@ -0,0 +1,9 @@
+name = "Trevor"
+print(f"Salve, {name}!")
+print(f"Vale, {name}!")
+
+#vs
+
+name = "Trevor"
+print("Salve,", name + "!")
+print("Vale,", name + "!")
---
+++ day1-hello-world.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print("Salve, Munde!")
+print("My name is Trevor")
